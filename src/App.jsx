5c1bde3e76015b0b240940ea1a8df46f9609b178
Muhammad Daffa Dinaya <13518141@std.stei.itb.ac.id>
2023-01-07 18:05:20 +0700

    feat: change to react js, vite, tauri

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [consumerConfig, setConsumerConfig] = useState({});
+  const [publisherConfig, setPublisherConfig] = useState({});
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <div className="container">
+      <h1>Welcome to Springhead Tauri!</h1>
+      <h4>Client Visualizer for Springhead</h4>
+
+      <div className="row">
+        <div className="container">
+          <div className="row">
+            <div>
+              <input
+                id="greet-input"
+                onChange={(e) => setName(e.currentTarget.value)}
+                placeholder="Enter a name..."
+              />
+              <button type="button" onClick={() => greet()}>
+                Greet
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div>
+              <input
+                id="greet-input"
+                onChange={(e) => setName(e.currentTarget.value)}
+                placeholder="Enter a name..."
+              />
+              <button type="button" onClick={() => greet()}>
+                Greet
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
